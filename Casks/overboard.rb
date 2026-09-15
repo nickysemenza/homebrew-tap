da7a7c005a124ae37351ab67b485556fa6a5465c
Nicky Semenza <nicky@nickysemenza.com>
2026-09-15 21:46:43 +0000

    overboard 0.7.0

--- Casks/overboard.rb
+++ Casks/overboard.rb
@@ -1,9 +1,9 @@
 cask "overboard" do
-  version "0.6.0"
+  version "0.7.0"
   # `version` and `sha256` are bumped by the Release workflow in
   # https://github.com/nickysemenza/overboard after each release's zip is
   # uploaded; don't edit them by hand.
-  sha256 "0ddcd8d00d1c323e6a1ec642f859ce50ca954c793237ef4b9739d1c7c0ed509e"
+  sha256 "7b897087ee1d06e632cb47b3d81dc4ebb507b78d18c4d45d204e150042dc95c2"
 
   url "https://github.com/nickysemenza/overboard/releases/download/v#{version}/Overboard-#{version}.zip"
   name "Overboard"
